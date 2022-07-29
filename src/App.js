57c35d10498bf45c3e2306618d3cbbbcd0b5591c
janmesh799 <janmesh799@gmail.com>
2022-07-29 16:16:10 +0530

    made navbar with routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Rent from "./Components/Rent/Rent";
+import Navbar from "./Components/Navbar/Navbar";
+import Buy from "./Components/Buy/Buy";
+import Sell from "./Components/Sell/Sell";
 
 function App() {
   return (
     <div className="App">
-     
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/rent" element={<Rent />}></Route>
+          <Route path="/buy" element={<Buy />}></Route>
+          <Route path="/sell" element={<Sell />}></Route>
+          <Route path="/" element={<Rent />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
